ea87b047b435d54eb3846436c2072aff9fd7ec3f
Yp2310 <110105072+Yp2310@users.noreply.github.com>
2023-08-09 18:01:21 +0530

    Final Commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        This is an about component
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+         Consequatur soluta est voluptatem minus debitis repellendus odit ex! 
+         Facere doloribus voluptatem hic modi necessitatibus commodi provident laudantium? 
+         Ex ea corrupti cum.</p>
+    </div>
+  )
+}
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Footer = () => {
+  // let footerStyle = {
+  //   position: "relative",    // a way to style a footer
+  //   top: "100vh",
+  //   width: "100%",  
+  // }
+  return (
+    <footer className="bg-dark text-light py-3" >
+      <p className="text-center">
+        Copywrite &copy; MyTodosList.com
+      </p>
+    </footer>
+  )
+}
